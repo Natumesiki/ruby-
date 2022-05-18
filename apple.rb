d9b4d301a282d9e033043051d9479cd77e4c1ee3
Natumesiki <102804114+Natumesiki@users.noreply.github.com>
2022-05-18 11:56:03 +0000

    elsif,else

--- apple.rb
+++ apple.rb
@@ -1,7 +1,13 @@
-apple = "Nagano"
+apple = "Yamanashi"
 
 if apple == "Aomori"
   puts "このリンゴは青森県産です。"
 elsif apple == "Nagano"
   puts "このリンゴは青森県産ではなく、長野県産です。"
 else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
+
+# elseはifの条件に当てはまらなかった場合の処理を設定
+# elsif を使うことで条件・処理を追加することが可能
+# elsifはいくつでも追加可能
