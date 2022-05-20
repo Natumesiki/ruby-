aac03b4071f18869683aee1c2405be8fe5872409
Natumesiki <102804114+Natumesiki@users.noreply.github.com>
2022-05-20 14:02:14 +0000

    レシーバself

--- new.rb
+++ new.rb
@@ -1,19 +1,23 @@
 # インスタンスメソッド
 class Car
- def turn(direction) 
- puts "#{direction}に曲がります。"
+ def move(direction,distance) 
+　self.turn(direction)
+　self.run(distance)
  end
-
+ 
+ def turn(direction)
+   puts "#{direction}に曲がります。"
+ end
+  
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
 car = Car.new
-car.turn("右")
+car.move("右",5)
 
-car = Car.new
-car.run(5)
+
 
 # クラスメソッド
 class Car
