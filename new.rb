ed35f1751ba4da34b75284f3af3244911d262653
Natumesiki <102804114+Natumesiki@users.noreply.github.com>
2022-05-20 02:28:58 +0000

    クラスメソッド

--- new.rb
+++ new.rb
@@ -1,3 +1,4 @@
+# インスタンスメソッド
 class Car
  def turn(direction) 
  puts "#{direction}に曲がります。"
@@ -14,3 +15,18 @@ car.turn("右")
 car = Car.new
 car.run(5)
 
+# クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+
+# ポイントなのは、 selfは、
+# クラス自身のことを示しているので
+# self.runは、Car.runを表しています。
+# インスタンスメソッドとクラスメソッドは、
+# 別々の引数を設定したので
+# 実行結果が変わります
